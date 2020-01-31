ec8a739e89d4a22e8146e0a1d619c39708d1eba8
Christof Sirk <christof.sirk@gmail.com>
2020-01-31 21:00:46 +0100

    Add index.js, index.html and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  externals: {},
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
